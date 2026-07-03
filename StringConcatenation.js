dd895fbe807be922f368a17ce5f1aa75dd892eaa
Saranya <saranyanallusamyr@gmail.com>
2026-07-02 19:18:46 -0700

    Added string concatenation

---
+++ StringConcatenation.js
@@ -0,0 +1,19 @@
+// string concatenation
+
+let s1 = "Hello";
+let s2 = "Javascript";
+
+console.log(s1 + "  " + s2);
+
+let a = 10;
+let b = 20;
+let x = 'hello';
+let y = ' world';
+
+// string concatenation starts from left to right
+
+console.log(a+b); //30
+console.log(x+y); //hello world
+console.log(a+b+x+y); //30hello world
+console.log(x+y+a+b); //hello world1020
+console.log(a+b+x+y+a+b); //30hello world1020
